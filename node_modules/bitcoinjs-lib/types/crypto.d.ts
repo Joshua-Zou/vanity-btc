91be4da70be053179dca10a6ba866ae70c950a96
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2021-06-27 15:46:49 -0700

    Delete node_modules directory

--- node_modules/bitcoinjs-lib/types/crypto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function ripemd160(buffer: Buffer): Buffer;
-export declare function sha1(buffer: Buffer): Buffer;
-export declare function sha256(buffer: Buffer): Buffer;
-export declare function hash160(buffer: Buffer): Buffer;
-export declare function hash256(buffer: Buffer): Buffer;
